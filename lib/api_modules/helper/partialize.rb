2da9d225cac7451647e298e304217ca793dd672c
Zenn <app.zenn@gmail.com>
2019-02-13 17:39:12 +0800

    add helper partialize module

---
+++ lib/api_modules/helper/partialize.rb
@@ -0,0 +1,24 @@
+module ApiModules
+  module Helper
+    module Partialize
+      def partialize(json, instance)
+        return if instance.nil?
+        klass = instance.class.name.underscore
+        json.partial! partial_path(klass), klass.split('/').last.intern => instance
+      end
+
+      private
+
+      # @return [String] バーチャルパスを基に組み立てたviewパーシャルパス
+      # @example
+      #  `sample/api/v1/books/show` => `sample/api/v1/models/book`
+      def partial_path(klass)
+        "#{_path}models/#{klass}"
+      end
+
+      def _path
+        @path ||= @virtual_path.gsub(/#{LAST_TWO_PATH_ELEMENTS}/o, '')
+      end
+    end
+  end
+end
