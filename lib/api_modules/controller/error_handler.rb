6991e8b0c3da2eefee510d7bc1d9235063710dc5
Zenn <app.zenn@gmail.com>
2019-03-10 20:36:33 +0900

    feat error_handler: add exceptin to before_server_error arguments

--- lib/api_modules/controller/error_handler.rb
+++ lib/api_modules/controller/error_handler.rb
@@ -11,12 +11,12 @@ module ApiModules
       private
 
       def server_error(exception = nil)
-        before_server_error
+        before_server_error(exception)
         render json: { type: 'unexpected_error' }, status: :inernal_server_error
       end
 
       # Implement before_server_error if you want to add implementation before server error
-      def before_server_error; end
+      def before_server_error(exception); end
     end
   end
 end
