c0be569c0163270122b628e80d68e45272c7365f
Zenn <app.zenn@gmail.com>
2019-10-06 00:36:54 +0900

    domain_handler: add handle_domain_execute

--- lib/api_modules/controller/domain_handler.rb
+++ lib/api_modules/controller/domain_handler.rb
@@ -10,6 +10,14 @@ module ApiModules
           render_domain_error domain
         end
       end
+
+      def handle_domain_execute(domain)
+        if result = domain.execute
+          yield result
+        else
+          render_domain_error domain
+        end
+      end
     end
   end
 end
