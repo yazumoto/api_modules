a835067fd8b1bbf5b31bc8de06619473131c28dc
Zenn <app.zenn@gmail.com>
2020-06-04 13:28:20 +0900

    fix 500 error bug

--- lib/api_modules/controller/error_handler.rb
+++ lib/api_modules/controller/error_handler.rb
@@ -12,7 +12,7 @@ module ApiModules
 
       def server_error(exception = nil)
         before_server_error(exception)
-        render json: { type: 'unexpected_error' }, status: :inernal_server_error
+        render json: { type: 'unexpected_error' }, status: :internal_server_error
       end
 
       # Implement before_server_error if you want to add implementation before server error
