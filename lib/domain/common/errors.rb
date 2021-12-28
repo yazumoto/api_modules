ca20a24a9b105cf73d737bb728709e8c86afb959
Yusuke Sasaki <yusuke.sasaki@playground.live>
2021-12-28 15:00:28 +0900

    fix: In Rails 6.1, `errors` is an array of Error

--- lib/domain/common/errors.rb
+++ lib/domain/common/errors.rb
@@ -2,8 +2,8 @@ module Domain
   module Common
     class Errors < ActiveModel::Errors
       def add_model_error(model, error_prefix = nil)
-        model.errors.each do |key, val|
-          add("#{error_prefix}#{key}", val)
+        model.errors.each do |error|
+          add("#{error_prefix}#{error.attribute}", error.message)
         end
       end
     end
